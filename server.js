894b6bddb6e26fff20e0a277e3ffc2361658874f
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2022-01-06 22:09:16 -0500

    test cache

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const database  = require( 'ronin-database' )
 
 const server = ronin.server()
 
-
 database.connect( process.env.CONNECTIONSTRING )
 server.use( '/foo', (req, res) => {
     return res.json({ "foo": "bar" })
